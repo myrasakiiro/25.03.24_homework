e7f653c69cd353462e54dfa5cb28fe173a123687
Viktoria <viktoruytarhova@gmail.com>
2024-03-28 09:41:56 +0200

    2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Задача 1: 
+// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+
+// a=50 => нет
+// a=7 => нет
+// a=322 => да
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 7 == 0 && number % 23 == 0)
+{
+    System.Console.Write("Да");
+}
+else
+{
+    System.Console.Write("Нет");
+}
